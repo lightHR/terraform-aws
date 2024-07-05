5352fb8dff41ddb04cc1d600fc047353a3fe2c0d
lightHR <rajkumar.aut@gmail.com>
2024-07-05 23:38:12 +0000

    route tables and internet gateway

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "networking-vpc" {
   source        = "./networking-vpc"
   vpc_cidr      = "10.123.0.0/16"
   public_sn_count = 2
-  private_sn_count = 5
+  private_sn_count = 3
   max_subnets = 20
   public_cidrs  = [for i in range(2, 255, 2) : cidrsubnet("10.123.0.0/16", 8, i)]
   private_cidrs = [for i in range(1, 255, 2) : cidrsubnet("10.123.0.0/16", 8, i)]
